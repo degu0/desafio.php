50a16f529be3ecf063165918cfdf8741d78679af
dey_gu <87346972+degu0@users.noreply.github.com>
2022-03-24 16:03:17 -0300

    Create desafio.php

---
+++ desafio.php
@@ -0,0 +1,53 @@
+<?php
+
+$array = [6, 30, 88, 79, 90, 35, 75, 84, 77, 32, 2, 90, 84, 90, 92, 34, 76, 4, 78, 73, 41];
+$arrayPar = [];
+$arrayImpar = [];
+
+$contador = 0;
+while ($contador < 21){
+    $numeroArray = $array[$contador];
+    if ($numeroArray % 2 == 0) {
+        $arrayPar_push($numeroArray);
+        $contador++;
+    }else if ($numeroArray % 2 != 0){
+        $arrayImpar_push($numeroArray);
+        $contador++;
+    }    
+};
+
+$contador = 0;
+$valor1 = 0;
+$valor2 = null;
+
+while($contador <= count($arrayPar)) {
+    if($arrayPar[$contador] > $valor1){
+        $valor1 = $arrayPar[$contador];
+    }else if ($arrayPar[$contador] > $valor1){
+        $valor2 = $arrayPar[$contador];
+    }else{
+        if($arrayPar[$contador] > $valor2) {
+            $valor2 = $arrayPar[$contador];
+        }
+    }
+    $contador++;
+};
+
+$contador = 0;
+$valorI1 = 0;
+$valorI2 = null;
+
+while($contador <= count($arrayImpar)) {
+    if($arrayImpar[$contador] > $valor1){
+        $valor1 = $arrayimpar[$contador];
+    }else if ($arrayImpar[$contador] > $valor1){
+        $valor2 = $arrayImpar[$contador];
+    }else{
+        if($arrayimpar[$contador] > $valor2) {
+            $valor2 = $arrayImpar[$contador];
+        }
+    }
+    $contador++;
+};
+
+echo ($valor1.$valor2);
